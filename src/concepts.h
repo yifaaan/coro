ca5fc89077be76b24c83e3b5127d3ab175239333
yifaaan <0219lyf@gmail.com>
2026-05-09 11:02:34 +0800

    Add coroutine support with concepts for awaiters and awaitables

---
+++ src/concepts.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <coroutine>
+#include <type_traits>
+#include <utility>
+
+#include "get_awaiter.h"
+#include "type_traits.h"
+
+namespace coro::detail {
+
+// 提取await_suspend参数的类型
+template <typename Ret, typename T, typename Handle>
+Handle FuncArg(Ret (*)(Handle));
+
+template <typename Ret, typename T, typename Handle>
+Handle FuncArg(Ret (T::*)(Handle));
+
+template <typename Ret, typename T, typename Handle>
+Handle FuncArg(Ret (T::*)(Handle) &);
+
+template <typename Ret, typename T, typename Handle>
+Handle FuncArg(Ret (T::*)(Handle) &&);
+
+template <typename Ret, typename T, typename Handle>
+Handle FuncArg(Ret (T::*)(Handle) const);
+
+template <typename Ret, typename T, typename Handle>
+Handle FuncArg(Ret (T::*)(Handle) const&);
+
+template <typename Ret, typename T, typename Handle>
+Handle FuncArg(Ret (T::*)(Handle) const&&);
+
+// 约束 await_suspend 的返回类型只能是协程标准允许的几种
+template <typename T>
+concept AwaitSuspendReturnType = std::is_void_v<T> || std::is_same_v<T, bool> ||
+                                 is_specialization_of<T, std::coroutine_handle>;
+
+}  // namespace coro::detail
+
+namespace coro {
+
+/*
+void await_suspend(std::coroutine_handle<>);
+std::coroutine_handle<> await_suspend(std::coroutine_handle<Promise>);
+*/
+template <typename T>
+concept Awaiter = requires(
+    T&& t, decltype(detail::FuncArg(
+               &std::remove_reference_t<T>::await_suspend)) await_suspend_arg) {
+  // await_ready() -> bool
+  { std::forward<T>(t).await_ready() } -> std::convertible_to<bool>;
+  // the await_suspend's arg
+  { await_suspend_arg } -> std::convertible_to<std::coroutine_handle<>>;
+  // the await_suspend's return type
+  { std::forward<T>(t).await_suspend() } -> detail::AwaitSuspendReturnType;
+  std::forward<T>(t).await_resume();
+};
+
+// Awaiter T的 await_resume() 的返回类型必须是Value
+template <typename T, typename Value>
+concept AwaiterOf = Awaiter<T> && requires(T&& t) {
+  { std::forward<T>(t).await_resume() } -> std::same_as<Value>;
+};
+
+// 可以被 co_await
+template <typename T>
+concept Awaitable = requires(T&& t) {
+  { detail::GetAwaiter(std::forward<T>(t)) } -> Awaiter;
+};
+
+
+// T 可以被 co_await，并且 co_await T 的结果类型是 Value
+template <typename T, typename Value>
+concept AwaitableOf = Awaitable<T> && requires(T&& t) {
+  { detail::GetAwaiter(std::forward<T>(t)) } -> AwaiterOf<Value>;
+};
+
+}  // namespace coro
---
+++ src/type_traits.h
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace coro::detail {
+
+template <typename T, template <typename...> typename Type>
+constexpr auto is_specialization_of = false;
+
+template <typename... Prams, template <typename...> typename Type>
+constexpr auto is_specialization_of<Type<Prams...>, Type> = true;
+
+}  // namespace coro::detail
+
+namespace coro {
+
+template <typename T, template <typename...> typename Template>
+concept specialization_of = detail::is_specialization_of<T, Template>;
+
+template <typename T>
+struct remove_rvalue_reference {
+  using type = T;
+};
+
+template <typename T>
+struct remove_rvalue_reference<T&&> {
+  using type = T;
+};
+
+template <typename T>
+using remove_rvalue_reference_t = remove_rvalue_reference<T>::type;
+
+}  // namespace coro
